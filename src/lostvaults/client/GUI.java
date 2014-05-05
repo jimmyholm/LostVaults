027c8105a067294a9694c9e5984ce32a2566fc1d
Anna <anna.fm.nilsson@gmail.com>
2014-05-05 14:58:27 +0200

    GUIFIX

--- src/lostvaults/client/GUI.java
+++ src/lostvaults/client/GUI.java
@@ -23,7 +23,7 @@ public class GUI {
     JTextArea exits = new JTextArea();
     JLabel stats = new JLabel();
     JFrame window = new JFrame("The Lost Vaults");
-    
+
     
     Color darkBackground = new Color(0x800000);
     Color lightBackground = new Color(0xDAC8A3);
@@ -36,9 +36,8 @@ public class GUI {
     Font bigFont = new Font("Serif", Font.BOLD + Font.ITALIC, 15);
 
     public String getName() { return name; }
-    public GUI() {
 
-				
+    public GUI() {				
 	// Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	screenWidth = 1000; // screenSize.width;
 	screenHeight = 600; // screenSize.height;
@@ -57,131 +56,50 @@ public class GUI {
 	dynamicInfoScroll.setBorder(BorderFactory.createEmptyBorder());
 
 
-
-
-
 	stats.setText("HP: 20/20 \t Food: 30/30");
 	stats.setFont(font);
 	stats.setForeground(lightTextColor);
 
 
-	dungeonPlayers.setFont(font);
-	dungeonPlayers.setForeground(textColor);
-	dungeonPlayers.setBackground(mediumBackground);
-	dungeonPlayers.setEditable(false);
-	JScrollPane dungeonPlayersScroll = new JScrollPane(dungeonPlayers); 
-	dungeonPlayersScroll.setBorder(BorderFactory.createEmptyBorder());
-	JLabel dungeonPlayersLabel = new JLabel("Players in Dungeon: ");
-	dungeonPlayersLabel.setForeground(textColor);
-	dungeonPlayersLabel.setFont(bigFont);
-	JPanel dungeonPlayersPanel = new JPanel(new BorderLayout());
-	dungeonPlayersPanel.setPreferredSize(new Dimension(0, screenHeight/5));
-	dungeonPlayersPanel.setBackground(darkMediumBackground);
-	dungeonPlayersPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
-	dungeonPlayersPanel.add(dungeonPlayersLabel, BorderLayout.NORTH);
-	dungeonPlayersPanel.add(dungeonPlayersScroll, BorderLayout.CENTER);
-
-
-
-	roomPlayers.setFont(font);
-	roomPlayers.setForeground(textColor);
-	roomPlayers.setBackground(mediumBackground);	
-	roomPlayers.setEditable(false);
-	JScrollPane roomPlayersScroll = new JScrollPane(roomPlayers); 
-	roomPlayersScroll.setBorder(BorderFactory.createEmptyBorder());
-	JLabel roomPlayersLabel = new JLabel("Players in Room: ");
-	roomPlayersLabel.setForeground(textColor);
-	roomPlayersLabel.setFont(bigFont);
-	JPanel roomPlayersPanel = new JPanel(new BorderLayout());
-	roomPlayersPanel.setPreferredSize(new Dimension(0, screenHeight/5));
-	roomPlayersPanel.setBackground(darkMediumBackground);
-	roomPlayersPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
-	roomPlayersPanel.add(roomPlayersLabel, BorderLayout.NORTH);
-	roomPlayersPanel.add(roomPlayersScroll, BorderLayout.CENTER);
-
-
-	others.setFont(font);
-	others.setForeground(textColor);
-	others.setBackground(mediumBackground);
-	others.setEditable(false);
-	JScrollPane othersScroll = new JScrollPane(others); 
-	othersScroll.setBorder(BorderFactory.createEmptyBorder());
-	JLabel othersLabel = new JLabel("Others in Room: ");
-	othersLabel.setForeground(textColor);
-	othersLabel.setFont(bigFont);
-	JPanel othersPanel = new JPanel(new BorderLayout());
-	othersPanel.setPreferredSize(new Dimension(0, screenHeight/5));
-	othersPanel.setBackground(darkMediumBackground);
-	othersPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
-	othersPanel.add(othersLabel, BorderLayout.NORTH);
-	othersPanel.add(othersScroll, BorderLayout.CENTER);
-
-
+	JPanel dungeonPlayersPanel = createRightBox(dungeonPlayers, "Players in Dungeon: ");	
+	JPanel roomPlayersPanel = createRightBox(roomPlayers, "Players in Room: ");
+	JPanel othersPanel = createRightBox(others, "Others in Room: ");
+	JPanel itemsPanel = createRightBox(items, "Items in Room: ");
 	
-	items.setFont(font);
-	items.setForeground(textColor);
-	items.setBackground(mediumBackground);
-	items.setEditable(false);
-	JScrollPane itemsScroll = new JScrollPane(items); 
-	itemsScroll.setBorder(BorderFactory.createEmptyBorder());
-	JLabel itemsLabel = new JLabel("Items in Room: ");
-	itemsLabel.setForeground(textColor);
-	itemsLabel.setFont(bigFont);
-	JPanel itemsPanel = new JPanel(new BorderLayout());
-	itemsPanel.setPreferredSize(new Dimension(0, screenHeight/5));
-	itemsPanel.setBackground(darkMediumBackground);
-	itemsPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
-	itemsPanel.add(itemsLabel, BorderLayout.NORTH);
-	itemsPanel.add(itemsScroll, BorderLayout.CENTER);
-	
-
 	exits.setFont(font);
 	exits.setForeground(textColor);
 	exits.setBackground(mediumBackground);
 	exits.setEditable(false);
-
 	JLabel exitsLabel = new JLabel("Exits in Room: ");
 	exitsLabel.setForeground(textColor);
 	exitsLabel.setFont(bigFont);
 	JPanel exitsPanel = new JPanel(new BorderLayout());
-	exitsPanel.setPreferredSize(new Dimension(0, screenHeight/5));
+	exitsPanel.setPreferredSize(new Dimension(0, 50));
 	exitsPanel.setBackground(darkMediumBackground);
 	exitsPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
 	exitsPanel.add(exitsLabel, BorderLayout.NORTH);
 	exitsPanel.add(exits, BorderLayout.CENTER);	
 
-
-	JPanel _1 = new JPanel(new BorderLayout());
-	_1.add(dungeonPlayersPanel, BorderLayout.NORTH);
-	_1.add(roomPlayersPanel, BorderLayout.CENTER);
-
-	JPanel _2 = new JPanel(new BorderLayout());
-	_2.add(_1, BorderLayout.NORTH);
-	_2.add(othersPanel, BorderLayout.CENTER);
-
-	JPanel _3 = new JPanel(new BorderLayout());
-	_3.add(_2, BorderLayout.NORTH);
-	_3.add(itemsPanel, BorderLayout.CENTER);
-
-
-	JPanel staticInfo = new JPanel(new BorderLayout());	
-	staticInfo.add(_3, BorderLayout.NORTH);
-	staticInfo.add(exitsPanel, BorderLayout.CENTER);
+	JPanel staticInfo = new JPanel(new GridLayout(0, 1));
+	staticInfo.setBackground(darkBackground);
+	staticInfo.add(dungeonPlayersPanel);
+	staticInfo.add(roomPlayersPanel);
+	staticInfo.add(othersPanel);
+	staticInfo.add(itemsPanel);
 	
-
 	JPanel rightPanel = new JPanel(new BorderLayout());
-	rightPanel.setPreferredSize(new Dimension(screenWidth / 3, 0));
+	//rightPanel.setPreferredSize(new Dimension(window.getSize().width / 3, 0));
 	rightPanel.setBackground(darkBackground);
 	rightPanel.add(stats, BorderLayout.NORTH);
 	rightPanel.add(staticInfo, BorderLayout.CENTER);
-	
-	
+	rightPanel.add(exitsPanel, BorderLayout.SOUTH);
 
-
-	JPanel mainPanel = new JPanel(new BorderLayout());
-	mainPanel.add(dynamicInfoScroll, BorderLayout.CENTER);
-	mainPanel.add(rightPanel, BorderLayout.EAST);
-
+	JSplitPane mainPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, dynamicInfoScroll, rightPanel);
+	mainPanel.setResizeWeight(0.85);
+	mainPanel.setBackground(darkBackground);
+	mainPanel.setBorder(null);
+	//	mainPanel.add(dynamicInfoScroll, BorderLayout.CENTER);
+	//	mainPanel.add(rightPanel, BorderLayout.EAST);
 
 
 	JLabel commandLabel = new JLabel("Command: ");
@@ -203,7 +121,6 @@ public class GUI {
 	command.add(commandLabel, BorderLayout.WEST);
 	command.add(commandInputField, BorderLayout.CENTER);
 
-
 	window.add(command, BorderLayout.SOUTH);
 	window.add(mainPanel, BorderLayout.CENTER);
 	window.setLocationRelativeTo(null);
@@ -211,6 +128,30 @@ public class GUI {
 
 	new LogInPopUp(window);
     }
+
+  
+    public JPanel createRightBox(JTextArea c, String label) {
+	c.setFont(font);
+	c.setForeground(textColor);
+	c.setBackground(mediumBackground);
+	c.setEditable(false);
+	JScrollPane cScroll = new JScrollPane(c); 
+	cScroll.setBorder(BorderFactory.createEmptyBorder());
+	JLabel cLabel = new JLabel(label);
+	cLabel.setForeground(textColor);
+	cLabel.setFont(bigFont);
+	JPanel cPanel = new JPanel(new BorderLayout());
+	cPanel.setBackground(darkMediumBackground);
+	cPanel.setBorder(BorderFactory.createLineBorder(darkBackground, 2));
+	cPanel.add(cLabel, BorderLayout.NORTH);
+	cPanel.add(cScroll, BorderLayout.CENTER);
+	return cPanel;
+    }
+    
+
+
+
+
 
     public class LogInPopUp extends JDialog implements ActionListener {
 	
@@ -297,13 +238,12 @@ public class GUI {
 	    //String pwd = passwordInput.getText();
 	    //String ip = IPInput.getText();
 	    user = user.replace(" ", "");
-	    playGameCommunication.sendMessage("LOGIN " + user);
+	    playGameCommunication.sendMessage("LOGIN " + user); /// Obs, ändra till caps
 	    window.setVisible(true);
 	    dispose();
 
 	}
     }
-
 
 
 
