48b33c095ddfc434d2ee0dec7942a3090ee9d0c8
F-L <fredrik.mejl@gmail.com>
2014-05-05 15:45:52 +0000

    GL HF

--- src/lostvaults/server/Player.scala
+++ src/lostvaults/server/Player.scala
@@ -18,19 +18,19 @@ class Player extends Actor {
     case Received(msg) => {
       connection = sender
       val decodedMsg = msg.decodeString(java.nio.charset.Charset.defaultCharset().name())
-      if (Parser.findWord(decodedMsg, 0) == "Login") {
+      if (Parser.findWord(decodedMsg, 0) == "LOGIN") {
         name = Parser.findWord(decodedMsg, 1)
         PMap ! PMapIsOnline(name, decodedMsg)
       }
     }
     case PMapIsOnlineResponse(response, purpose) =>
       {
-        if (Parser.findWord(purpose, 0) == "Login") {
+        if (Parser.findWord(purpose, 0) == "LOGIN") {
           if (response) {
-            connection ! Write(ByteString("LoginFail"))
+            connection ! Write(ByteString("LOGINFAIL"))
             context stop self
           } else {
-            connection ! Write(ByteString("LoginOk"))
+            connection ! Write(ByteString("LOGINOK"))
             PMap ! PMapAddPlayer(name, self)
             dungeon = Main.City.get
             dungeon ! GameAddPlayer(name)
@@ -41,54 +41,54 @@ class Player extends Actor {
       def LoggedIn: Receive = {
         case Received(msg) => {
           val decodedMsg = msg.decodeString(java.nio.charset.Charset.defaultCharset().name())
-          val action = Parser.findWord(decodedMsg, 0)
+          val action = Parser.findWord(decodedMsg, 0).toUpperCase
           action match {
-            case "Say" => {
+            case "SAY" => {
               dungeon ! GameSay(name, Parser.findRest(decodedMsg, 0))
             }
-            case "Whisper" => {
+            case "WHISPER" => {
               whisperTo = Parser.findWord(decodedMsg, 1)
               whisperMsg = Parser.findRest(decodedMsg, 1)
               PMap ! PMapGetPlayer(whisperTo, decodedMsg)
             }
-            case "LogOut" => {
+            case "LOGOUT" => {
               connection ! Write(ByteString("Bye"))
               connection ! Close
             }
             case _ => {
-              connection ! Write(ByteString("System I have no idea what you're wanting to do."))
+              connection ! Write(ByteString("SYSTEM I have no idea what you're wanting to do."))
             }
           }
         }
         case GamePlayerEnter(name) => {
-          connection ! Write(ByteString("System Player " + name + " has entered the dungeon"))
+          connection ! Write(ByteString("SYSTEM Player " + name + " has entered the dungeon"))
         }
         case GamePlayerLeft(name) => {
-          connection ! Write(ByteString("System Player " + name + " has left the dungeon"))
+          connection ! Write(ByteString("SYSTEM Player " + name + " has left the dungeon"))
         }
         case GameSay(name, msg) => {
-          connection ! Write(ByteString("Say " + name + " " + msg))
+          connection ! Write(ByteString("SAY " + name + " " + msg))
         }
         case GameWhisper(from, to, msg) => {
-          connection ! Write(ByteString("Whisper " + from + " " + to + " " + msg))
+          connection ! Write(ByteString("WHISPER " + from + " " + to + " " + msg))
         }
         case GameMoveToDungeon(dungeon) => {
           this.dungeon = dungeon
         }
         case GameSystem(msg) => {
-          connection ! Write(ByteString("System " + msg))
+          connection ! Write(ByteString("SYSTEM " + msg))
         }
         case PMapGetPlayerResponse(player, purpose) => {
-          val action = Parser.findWord(purpose, 0)
+          val action = Parser.findWord(purpose, 0).toUpperCase
           action match {
-            case "Whisper" => {
+            case "WHISPER" => {
               if (!player.isEmpty) {
                 val to = Parser.findWord(purpose, 1)
                 val msg = Parser.findRest(purpose, 1)
                 player.get ! GameWhisper(name, to, msg)
                 self ! GameWhisper(name, to, msg)
               } else
-                connection ! Write(ByteString("System No Such Player Online"))
+                connection ! Write(ByteString("SYSTEM No Such Player Online"))
             }
           }
         }
