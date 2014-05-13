e20806538c6c6cd6f9518f979cd7f84a7f98b130
Felix Farjsjo <fefa4089@fries.it.uu.se>
2014-05-13 14:20:06 +0200

    Added comments

--- src/lostvaults/server/Treasure.scala
+++ src/lostvaults/server/Treasure.scala
@@ -1,5 +1,8 @@
 package lostvaults.server
-
+/**
+ * The class Treasure is a constructor of findable treasures in the game.
+ *
+ */
 class Treasure (_name : String, _value : Int) extends Item (_name : String, _value : Int) {
 
 }
