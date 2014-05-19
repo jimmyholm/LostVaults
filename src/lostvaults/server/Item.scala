b48dd5bb596c59f87486b4a9dcc0f43e525057d0
philip-akerfeldt <Philip.akerfeldt@hotmail.com>
2014-05-19 13:13:38 +0200

    Added a few things to the item class that I find necessary

--- src/lostvaults/server/Item.scala
+++ src/lostvaults/server/Item.scala
@@ -1,3 +1,5 @@
+import scala.io.Source
+
 package lostvaults.server
 /**
  * The class item is a constructor for items in the game. An item is  
@@ -8,3 +10,18 @@ class Item (_name : String, _value : Int) {
 	val name = _name
 	var value = _value
 }
+	def readItemsFromFile() {
+      println("Following is the content read:" )
+
+      Source.fromFile("test.txt" ).foreach { 
+         print 
+      }
+	}
+      def createItems() {
+        
+      }
+      
+      def updateItems() {   
+      }
+
+
