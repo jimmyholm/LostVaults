4a9ea68d1ad07784c6c5a475e1a4571439e60abb
senilica@gmail.com <senilica@gmail.com>
2014-05-20 11:20:03 +0200

    Further preparation for database integration.

--- src/lostvaults/server/Item.scala
+++ src/lostvaults/server/Item.scala
@@ -1,29 +1,24 @@
-import scala.io.Source
-
 package lostvaults.server
+import scala.io.Source 
 /**
  * The class item is a constructor for items in the game. An item is  
  * composed of the name of the item, and the value of it.
- */
+ */ 
+class Item(_name: String, _value: Int) {
+  val name = _name
+  var value = _value
+  def readItemsFromFile() {
+    println("Following is the content read:")
 
-class Item (_name : String, _value : Int) {
-	val name = _name
-	var value = _value
-	
-	
+    Source.fromFile("test.txt").foreach {
+      print
+    }
+  }
+  def createItems() {
+
+  }
+
+  def updateItems() {
+  }
+
 }
-	def readItemsFromFile() {
-      println("Following is the content read:" )
-
-      Source.fromFile("test.txt" ).foreach { 
-         print 
-      }
-	}
-      def createItems() {
-        
-      }
-      
-      def updateItems() {   
-      }
-
-
