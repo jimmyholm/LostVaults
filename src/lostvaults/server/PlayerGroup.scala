48d7796df6a31bb6b2ffb8b2b2140c04fe76558f
senilica <senilica@users.noreply.github.com>
2014-06-08 10:35:42 +0200

    Update PlayerGroup.scala

--- src/lostvaults/server/PlayerGroup.scala
+++ src/lostvaults/server/PlayerGroup.scala
@@ -1,3 +1,8 @@
+/**
+ * PlayerGroup.scala
+ * @Author Felix Färsjö, Jimmy Holm, Fredrik Larsson, Anna Nilsson, Philip Åkerfeldt
+ * @Version 1.0
+ */
 package lostvaults.server
 import scala.collection.mutable.Set
 /**
