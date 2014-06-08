ed6cedfe926e1087f0475fa039bf2f0b85345f09
senilica <senilica@users.noreply.github.com>
2014-06-08 10:37:19 +0200

    Update Room.scala

--- src/lostvaults/server/Room.scala
+++ src/lostvaults/server/Room.scala
@@ -1,3 +1,8 @@
+/**
+ * Room.scala
+ * @Author Felix Färsjö, Jimmy Holm, Fredrik Larsson, Anna Nilsson, Philip Åkerfeldt
+ * @Version 1.0
+ */
 package lostvaults.server
 import akka.actor.{ Actor, ActorRef }
 
