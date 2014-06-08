2c704faa29b28d342af6a6bb73094d218b896679
senilica <senilica@users.noreply.github.com>
2014-06-08 10:25:45 +0200

    Update Combat.scala

--- src/lostvaults/server/Combat.scala
+++ src/lostvaults/server/Combat.scala
@@ -1,3 +1,8 @@
+/**
+ * CheatActor.scala
+ * @Author Felix Färsjö, Jimmy Holm, Fredrik Larsson, Anna Nilsson, Philip Åkerfeldt
+ * @Version 1.0
+ */
 package lostvaults.server
 import akka.actor.{ Actor, ActorRef, FSM, Props}
 import scala.concurrent.duration._
